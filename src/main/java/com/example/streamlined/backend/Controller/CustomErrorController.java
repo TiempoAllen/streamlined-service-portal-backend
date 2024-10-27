ba29cd48a85db174a639dae1a78c94fd72dfa967
franco <francodellap@gmail.com>
2024-10-27 14:25:08 +0800

    jakarta boi

--- src/main/java/com/example/streamlined/backend/Controller/CustomErrorController.java
+++ src/main/java/com/example/streamlined/backend/Controller/CustomErrorController.java
@@ -1,13 +1,13 @@
 package com.example.streamlined.backend.Controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class CustomErrorController implements ErrorController {
